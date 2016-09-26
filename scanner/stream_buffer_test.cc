a85554335d84da7cc06dc4d59e967f5cfcefe1c7
Hieu Le <trunghieule94@gmail.com>
2016-09-26 17:14:35 -0500

    Add unit tests for StreamBuffer

--- scanner/stream_buffer_test.cc
+++ scanner/stream_buffer_test.cc
@@ -12,8 +12,10 @@
 namespace truplc {
 namespace {
 
+// Test if StreamBuffer generates on specified input an expected sequence of
+// characters.
 void TestNextChar(const std::string& input,
-                 const std::vector<char> expected) {
+                  const std::vector<char>& expected) {
   std::istringstream ss(input);
   std::unique_ptr<Buffer> buffer = std::make_unique<StreamBuffer>(&ss);
   for (const char c : expected) {
@@ -22,55 +24,38 @@ void TestNextChar(const std::string& input,
 }
 
 TEST(StreamBufferTest, NextCharBasic) {
-  TestNextChar("foo", {'f', 'o', 'o', kEOFMarker});
+  TestNextChar("bool", {'b', 'o', 'o', 'l', kEOFMarker});
+  TestNextChar("int a;", {'i', 'n', 't', kSpace, 'a', ';', kEOFMarker});
+  TestNextChar("a = 3;", {'a', kSpace, '=', kSpace, '3', ';', kEOFMarker});
+  TestNextChar("if(a )", {'i', 'f', '(', 'a', kSpace, ')', kEOFMarker});
+  TestNextChar(";:(),=<>+-",
+               {';', ':', '(', ')', ',', '=', '<', '>', '+', '-', kEOFMarker});
 }
 
 TEST(StreamBufferTest, NextCharWithWhitespace) {
   TestNextChar("a b", {'a', kSpace, 'b', kEOFMarker});
-}
-
-TEST(StreamBufferTest, NextCharWithMultipleWhitespaces) {
   TestNextChar("a\n\nb", {'a', kSpace, 'b', kEOFMarker});
-}
-
-TEST(StreamBufferTest, NextCharWithPrecedingWhitespaces) {
   TestNextChar("\n\n\t a", {'a', kEOFMarker});
-}
-
-TEST(StreamBufferTest, NextCharWithInterveningWhitespaces) {
-  TestNextChar("a\n\nb", {'a', kSpace, 'b', kEOFMarker});
-}
-
-TEST(StreamBufferTest, NextCharWithTrailingWhitespaces) {
   TestNextChar("a \n\t ", {'a', kSpace, kEOFMarker});
-}
-
-TEST(StreamBufferTest, NextCharWithOnlyWhitespaces) {
   TestNextChar("  \n\n\t\t  ", {kEOFMarker});
 }
 
 TEST(StreamBufferTest, NextCharWithComments) {
   TestNextChar("f#abc def ghi\nb", {'f', kSpace, 'b', kEOFMarker});
-}
-
-TEST(StreamBufferTest, NextCharWithMultilineComments) {
   TestNextChar("a#foo quoz bar \n#bar\nb", {'a', kSpace, 'b', kEOFMarker});
-}
-
-TEST(StreamBufferTest, NextCharWithPrecedingComments) {
+  TestNextChar("#this #is #comment\na#abcxyz", {'a', kSpace, kEOFMarker});
+  TestNextChar("a#this\t\tis a\t\tcomment\nb", {'a', kSpace, 'b', kEOFMarker});
   TestNextChar("#this is a comment\na", {'a', kEOFMarker});
-}
-
-TEST(StreamBufferTest, NextCharWithTrailingComments) {
   TestNextChar("a#this is a comment", {'a', kSpace, kEOFMarker});
-}
-
-TEST(StreamBufferTEST, NextCharWithOnlyComments) {
   TestNextChar("#this is a comment", {kEOFMarker});
 }
 
+
 TEST(StreamBufferTest, NextCharWithMixingWhitespaceAndComments) {
   TestNextChar("a #foo bar\n b", {'a', kSpace, 'b', kEOFMarker});
+  TestNextChar("#foo bar\n\n\t  a b", {'a', kSpace, 'b', kEOFMarker});
+  TestNextChar(" a #foo bar \n \t   b #foo\n\t c#foo bar",
+               {'a', kSpace, 'b', kSpace, 'c', kSpace, kEOFMarker});
 }
 
 TEST(StreamBufferTest, NextCharWithEmptyStream) {
@@ -89,6 +74,16 @@ TEST(StreamBufferTest, NextCharWithLongInputStream) {
   std::vector<char> expected(input.begin(), input.end());
   expected.push_back(kEOFMarker);
   TestNextChar(input, expected);
+}
+
+TEST(StreamBufferTest, UnreadCharBasic) {
+  std::istringstream ss("a");
+  std::unique_ptr<Buffer> buffer = std::make_unique<StreamBuffer>(&ss);
+  char result = buffer->NextChar();
+  EXPECT_EQ(result, 'a');
+  EXPECT_EQ(buffer->NextChar(), kEOFMarker);
+  buffer->UnreadChar(result);
+  EXPECT_EQ(buffer->NextChar(), result);
 }
 
 }  // namespace
