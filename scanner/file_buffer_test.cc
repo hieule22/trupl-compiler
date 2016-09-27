1292c1898e198b6d48430698e15f23cb7d913380
Hieu Le <trunghieule94@gmail.com>
2016-09-27 15:06:45 -0500

    Test initializing FileBuffer with invalid filename

--- scanner/file_buffer_test.cc
+++ scanner/file_buffer_test.cc
@@ -3,23 +3,13 @@
 
 #include "scanner/file_buffer.h"
 
-#include <iostream>
-#include <utility>
-
 #include "gtest/gtest.h"
 
 namespace truplc {
 namespace {
 
-TEST(FileBufferTest, NextCharBasic) {
-  const std::string filename = "";
-  std::unique_ptr<Buffer> buffer = std::make_unique<FileBuffer>(filename);
-  char current = buffer->NextChar();
-  while (current != kEOFMarker) {
-    std::cout << current;
-    current = buffer->NextChar();
-  }
-  std::cout << std::endl;
+TEST(FileBufferDeathTest, ConstructWithIllegalFilename) {
+  EXPECT_DEATH({ FileBuffer buffer("Foo"); }, "");
 }
 
 }  // namespace
