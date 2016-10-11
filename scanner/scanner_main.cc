f6a4ba13f177e28d5f840799dc98fbf6cbbca533
Hieu Le <trunghieule94@gmail.com>
2016-10-11 01:16:02 -0500

    Minor fixes

--- scanner/scanner_main.cc
+++ scanner/scanner_main.cc
@@ -7,15 +7,15 @@
 #include <iostream>
 
 #include "scanner/scanner.h"
+#include "util/string_util.h"
 #include "util/text_colorizer.h"
 
 int main(int argc, char** argv) {
   char *filename;
   if (argc != 2) {
-    std::cerr <<
-        truplc::TextColorizer(truplc::TextColor::FG_RED) <<
-        "Usage: " << argv[0] << " <input file name>" <<
-        truplc::TextColorizer(truplc::TextColor::FG_DEFAULT) << std::endl;
+    truplc::TextColorizer::Print(
+        std::cerr, truplc::TextColorizer::kFGRedColorizer,
+        truplc::StrCat("Usage: ", argv[0], " <input file name>\n"));
     exit(EXIT_FAILURE);
   }
 
@@ -28,12 +28,13 @@ int main(int argc, char** argv) {
   do {
     token = scanner.NextToken();
     if (token->GetTokenType() == truplc::TokenType::kUnspecified) {
-      std::cerr <<
-          truplc::TextColorizer(truplc::TextColor::FG_RED) <<
-          "Error: NextToken() returned typeless token." <<
-          truplc::TextColorizer(truplc::TextColor::FG_DEFAULT) << std::endl;
+      truplc::TextColorizer::Print(
+          std::cerr, truplc::TextColorizer::kFGRedColorizer,
+          "Error: NextToken() returned typeless token.\n");
     } else {
-      std::cout << token->DebugString() << std::endl;
+      truplc::TextColorizer::Print(
+          std::cout, truplc::TextColorizer::kFGGreenColorizer,
+          truplc::StrCat(token->DebugString(), "\n"));
     }
   } while (token->GetTokenType() != truplc::TokenType::kEOF);
 
