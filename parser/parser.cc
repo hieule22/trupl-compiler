34fc18160070af8847a96b25f32ce732655c8cf3
Hieu Le <trunghieule94@gmail.com>
2016-11-23 06:46:35 -0600

    Implement API for Parser class

---
+++ parser/parser.cc
@@ -0,0 +1,22 @@
+// Implementation for Parser class.
+// Copyright 2016 Hieu Le.
+
+#include "parser/parser.h"
+
+namespace truplc {
+
+Parser::Parser(std::unique_ptr<Scanner> scanner) {
+  // TODO(hieule): Unimplemented constructor.
+}
+
+bool Parser::ParseProgram() {
+  // TODO(hieule): Unimplemented method.
+  return false;
+}
+
+bool Parser::HasNextToken() const {
+  // TODO(hieule): Unimplemented method.
+  return false;
+}
+
+}  // namespace truplc
